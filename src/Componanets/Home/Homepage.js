23111760fafd1adbae4f08e8986fd85c6fa8ff22
jasmin <jasmikdobariya@gmail.com>
2023-12-25 18:13:43 +0530

    change import speling

--- src/Componanets/Home/Homepage.js
+++ src/Componanets/Home/Homepage.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ImageSlider from "../Slider/Slider";
 import Arrivals from "../Arrivals/Arrivals";
-import Sealing from "../Seleng/Sealing";
 import Blog from "../Blog/Blog";
+import Sealing from "../Selling/Sealing";
 
 
 const Homepage = () => {
